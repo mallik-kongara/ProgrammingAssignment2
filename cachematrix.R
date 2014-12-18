d0d5766773c4015e3398d853bb0a645affe68660
Mallik Kongara <mallik.kongra@gmail.com>
2014-12-18 11:46:44 -0800

    Fix more typos in comments.

--- cachematrix.R
+++ cachematrix.R
@@ -28,7 +28,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## The following function calculates the inverse of the special "matrix" created
-## with the above function. However, it first checks to see if the inverse has
+## with the above function. However, it first checks to see if the inverse of
 ## the matrix has already been calculated. If so, it gets the inverse from the
 ## cache and skips the computation. Otherwise, it calculates the inverse of the
 ## matrix and sets the value of the inverse in the cache via the setinverse
