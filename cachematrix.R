69c78b0ce28404c36be6c439718c5d365f9abe48
Mallik Kongara <mallik.kongra@gmail.com>
2014-12-18 11:45:30 -0800

    Fix typo.

--- cachematrix.R
+++ cachematrix.R
@@ -27,7 +27,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-## The following function calculates the mean of the special "matrix" created
+## The following function calculates the inverse of the special "matrix" created
 ## with the above function. However, it first checks to see if the inverse has
 ## the matrix has already been calculated. If so, it gets the inverse from the
 ## cache and skips the computation. Otherwise, it calculates the inverse of the
